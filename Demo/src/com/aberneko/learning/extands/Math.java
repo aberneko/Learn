da1bb22ce73b14e93a714315ae7e52d66e0c9f2b
WorkAberneko <kurumi.06.10.t@gmail.com>
2025-12-16 16:02:36 +0300

    версия 2.0 (1.0)

--- Demo/src/com/aberneko/learning/extands/Math.java
+++ Demo/src/com/aberneko/learning/extands/Math.java
@@ -5,14 +5,4 @@ import java.util.HashMap;
 
 public class Math extends Teacher{
 
-    ArrayList<Integer> oth = new ArrayList<>();
-
-    public void alloth() {
-        for (int i = 0; i < 10);
-    }
-
-    @Override
-    public String alwrite() {
-        return "сидеть";
-    }
 }
